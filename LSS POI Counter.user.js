7f60ce2d51acec88be1cee11a7533a2d996f1b21
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-09-30 18:26:14 +0200

    Update LSS POI Counter.user.js
    -removed button because of Darkmodeproblem
    +added Shortcut to footer

--- LSS POI Counter.user.js
+++ LSS POI Counter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Leitstellenspiel POI Zähler
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Fügt einen Button hinzu, um POIs zu zählen und sie in einer Tabelle anzuzeigen.
+// @version      1.1
+// @description  Fügt einen Hyperlink im Footer hinzu, um POIs zu zählen und sie in einer Tabelle anzuzeigen.
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/
 // @grant        none
@@ -11,22 +11,11 @@
 (function() {
     'use strict';
 
-    // Funktion zum Erstellen des Buttons
-    function createButton() {
-        const newButton = document.createElement('button');
-        newButton.textContent = 'Zähle POIs';
-        newButton.style.position = 'fixed';
-        newButton.style.bottom = '10px';
-        newButton.style.left = '10px';
-        newButton.addEventListener('click', countAndDisplayPOIs);
-        document.body.appendChild(newButton);
-    }
-
     // Funktion zum Zählen der POIs und Anzeigen in einer Tabelle
     function countAndDisplayPOIs() {
         // Zählt die POIs und speichert sie in einem Objekt
         const poiCounts = map_pois_service.getMissionPoiMarkersArray().map(poi => poi.getTooltip().getContent()).reduce((a, b) => (a[b] = (a[b] ?? 0) + 1, a), {});
-        
+
         // Erstellt eine Tabelle für die Anzeige der POIs
         const poiTable = document.createElement('table');
         const headerRow = poiTable.insertRow();
@@ -49,6 +38,30 @@
         newWindow.document.body.appendChild(poiTable);
     }
 
-    // Ruft die Funktion zum Erstellen des Buttons auf
-    createButton();
+    // Funktion zum Erstellen des Hyperlinks im Footer
+    function createLink() {
+        // Erstelle ein neues Element für den "Zähle POIs" Link
+        var poiCounterLink = document.createElement('a');
+        poiCounterLink.href = '#'; // Setze die URL oder den Klick-Handler für den POI-Zähler hier ein
+        poiCounterLink.innerText = 'Zähle POIs';
+
+        // Erstelle ein neues Element für den Trennpunkt
+        var separator = document.createElement('span');
+        separator.innerText = ' · ';
+
+        // Finde das Element für "Impressum" im Footer
+    var impressumLink = document.querySelector('.footer.hidden-xs a[href="/impressum"]');
+
+        // Füge den "Zähle POIs" Link vor dem "Grafiksets" Link ein
+    if (impressumLink && impressumLink.parentNode) {
+        impressumLink.parentNode.insertBefore(poiCounterLink, impressumLink);
+        impressumLink.parentNode.insertBefore(separator, impressumLink);
+        }
+
+        // Füge einen Klick-Handler zum Zählen und Anzeigen der POIs hinzu
+        poiCounterLink.addEventListener('click', countAndDisplayPOIs);
+    }
+
+    // Ruft die Funktion zum Erstellen des Hyperlinks im Footer auf
+    createLink();
 })();
